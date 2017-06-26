08caf1e7e102b267ad6fe7f233ce20e890299fdf
Cerdic <cedric@yterium.com>
2017-06-26 12:51:49 +0200

    renseigner les montant_ht des echeances

--- inc/abos.php
+++ inc/abos.php
@@ -38,20 +38,23 @@ function abos_calculer_echeances_commande($id_commande){
 	$echeances = array(
 		0 => array(
 			'montant' => 0,
+			'montant_ht' => 0,
 			'nb' => 1
 		)
 	);
 
 	$details = sql_allfetsel('*', 'spip_commandes_details', 'id_commande=' . intval($id_commande), '', 'id_commandes_detail');
 	foreach ($details as $detail){
-		$prix = $detail['prix_unitaire_ht']*(1.0+$detail['taxe'])*$detail['quantite'];
+		$prix_ht = $detail['prix_unitaire_ht'] * $detail['quantite'];
+		$prix = $prix_ht * (1.0 + $detail['taxe']);
 		$echeances[0]['montant'] += $prix;
+		$echeances[0]['montant_ht'] += $prix_ht;
 		$echeances_type = '';
 
 		// trouver toutes les offres d'abonnement en renouvellement tacite dans la commande
 		// et calculer les echeances
 		// on ne prend en compte que les offres avec periode=1 (1 mois ou 1 an),
-		// et si plusieurs offres avec periodes de type differentes, seule la premiere rencontree sera prise en compte
+		// et si plusieurs offres avec periodes de type differentes, seule la premiere periode rencontree sera prise en compte
 		if ($detail['objet']=='abooffre'
 			and $offre = sql_fetsel('*', 'spip_abo_offres', 'id_abo_offre=' . intval($detail['id_objet']))
 		){
@@ -68,12 +71,15 @@ function abos_calculer_echeances_commande($id_commande){
 				){
 					$echeances_type = $type;
 					if (!isset($echeances[1])){
-						$echeances[1] = array('montant' => 0, 'nb' => 0);
+						$echeances[1] = array('montant' => 0, 'montant_ht' => 0, 'nb' => 0);
 					}
+					$prix_renouvellement_ht = $prix_ht;
 					$prix_renouvellement = $prix;
 					if (floatval($offre['prix_ht_renouvellement'])>0.01){
-						$prix_renouvellement = $offre['prix_ht_renouvellement']*(1.0+$detail['taxe'])*$detail['quantite'];
+						$prix_renouvellement_ht = $offre['prix_ht_renouvellement'] * $detail['quantite'];
+						$prix_renouvellement = $prix_renouvellement_ht * (1.0+$detail['taxe']);
 					}
+					$echeances[1]['montant_ht'] += $prix_renouvellement_ht;
 					$echeances[1]['montant'] += $prix_renouvellement;
 				}
 			}
@@ -82,11 +88,13 @@ function abos_calculer_echeances_commande($id_commande){
 		if ($echeances_type){
 			foreach ($echeances as $k => $echeance){
 				// on force en string pour la serialization qui sinon reintroduit des virgules non significatives
+				$echeances[$k]['montant_ht'] = (string)round($echeances[$k]['montant_ht'], 2);
 				$echeances[$k]['montant'] = (string)round($echeances[$k]['montant'], 2);
 			}
 
 			if (count($echeances)==2
-				and $echeances[0]['montant']==$echeances[1]['montant']
+				and $echeances[0]['montant'] == $echeances[1]['montant']
+				and $echeances[0]['montant_ht'] == $echeances[1]['montant_ht']
 			){
 				if ($echeances[0]['nb']>0 and $echeances[1]['nb']>0){
 					$echeances[0]['nb'] += $echeances[1]['nb'];
