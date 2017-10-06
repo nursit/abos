78031855704ad8199086972099040bbe2e97fdef
Matthieu Marcillaud <marcimat@rezo.net>
2017-10-06 11:34:00 +0200

    Des traductions supplémentaires en anglais.

--- lang/abonnement_en.php
+++ lang/abonnement_en.php
@@ -9,6 +9,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'ajouter_lien_abonnement' => 'Add this subscription',
 
+	// C
+	'confirmer_resilier_immediatement' => 'Terminate the subscription immediately? (all rights will be canceled)',
+	'confirmer_resilier_echeance' => 'Terminate at the due date?  (The user will be able to use his subscription until the due date)',
+	'confirmer_changement_abonne_uid' => 'You must make the corresponding manual change at your payment provider <tt>@presta@</tt>.',
+	'confirmer_changement_prix_echeance' => 'You must make the corresponding manual change at your payment provider <tt>@presta@</tt>.',
+	'confirmer_changement_date' => 'You must make the corresponding manual change at your payment provider <tt>@presta@</tt>.',
+
+	// D
+	'dont' => 'Including',
+
+	// E
+	'erreur_date_echeance_ou_fin_obli' => 'Spécify a due date or an end date',
+
+	// G
+	'gratuits_zero' => '0 free',
+	'gratuits_nb' => '@nb@ free',
+	'gratuits_un' => '1 free',
+
 	// I
 	'icone_creer_abonnement' => 'Add a subscription',
 	'icone_modifier_abonnement' => 'Modify this subscription',
@@ -18,18 +36,25 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_abonnements' => '@nb@ subscriptions',
 	'info_resilier_immediatement' => 'Terminate immediately',
 	'info_resilier_echeance' => 'Terminate at the due date',
-	'confirmer_resilier_immediatement' => 'Terminate the subscription immediately? (all rights will be canceled)',
-	'confirmer_resilier_echeance' => 'Terminate at the due date?  (The user will be able to use his subscription until the due date)',
-	'confirmer_changement_abonne_uid' => 'You must make the corresponding manual change at your payment provider <tt>@presta@</tt>.',
-	'confirmer_changement_prix_echeance' => 'You must make the corresponding manual change at your payment provider <tt>@presta@</tt>.',
-	'confirmer_changement_date' => 'You must make the corresponding manual change at your payment provider <tt>@presta@</tt>.',
-	'label_confirmer_modification' => 'Confirm modification?',
+	'info_statut_resilie' => 'Terminated',
+	'info_statut_impaye' => 'Unpaid',
+	'info_statut_ok' => 'OK',
+	'info_statut_prepa' => 'command',
+	'info_statut_erreur' => '(error)',
 
-	'erreur_date_echeance_ou_fin_obli' => 'Spécify a due date or an end date',
+	// N
+	'non_commences_zero' => '0 not started yet',
+	'non_commences_un' => '1 not started yet',
+	'non_commences_nb' => '@nb@ not started yet',
+
+	// P
+	'parraines_zero' => '0 sponsorred',
+	'parraines_un' => '1 sponsorred',
+	'parraines_nb' => '@nb@ sponsorred',
 
 	// L
 	'label_abonne_uid' => 'Subscriber uid',
-	'label_log' => 'Log',
+	'label_confirmer_modification' => 'Confirm modification?',
 	'label_confirm' => 'Confirm',
 	'label_message' => 'Message client',
 	'label_compte' => 'Account',
@@ -47,18 +72,27 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_id_auteur' => 'Author Id',
 	'label_id_transaction_echeance' => 'Transaction term Id',
 	'label_id_transaction_essai' => 'Transaction trial Id',
+	'label_log' => 'Log',
+	'label_mois_de_fin' => 'Ending month',
 	'label_mode' => 'Mode',
 	'label_mode_echeance' => 'Term mode',
 	'label_mode_paiement' => 'Payment mode',
+	'label_nombre' => 'Count',
+	'label_non_renouveles' => 'Not renewed',
 	'label_offre' => 'Offer',
+	'label_total' => 'Total',
 	'label_prix_echeance' => 'Term price',
+	'label_renouveles' => 'Renewed',
 
 	// R
 	'retirer_lien_abonnement' => 'Remove this subscription',
 	'retirer_tous_liens_abonnements' => 'Remove all subscriptions',
 
 	// T
+	'texte_abonnements_en_cours' => 'Subscriptions in progress',
+	'texte_abonnements_finis_selon_mois' => 'Subscriptions finished according to month',
 	'texte_ajouter_abonnement' => 'Add a subscription',
+	'texte_aucune_fin_abonnement_nb_mois' => 'No subscription ends within @nb@ months.',
 	'texte_changer_statut_abonnement' => 'This subscription is:',
 	'texte_creer_associer_abonnement' => 'Create and link a subscription',
 	'texte_statut_prepa' => 'Command',
@@ -67,17 +101,16 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_abonnement' => 'Subscription',
 	'titre_abonnements' => 'Subscriptions',
 	'titre_abonnements_rubrique' => 'Subscriptions from this section',
+	'titre_cadeaux' => 'Gifts',
+	'titre_encaissements_par_durees' => 'Receipts by durations',
 	'titre_langue_abonnement' => 'Subscription\'s language',
 	'titre_logo_abonnement' => 'Subscription\'s logo',
 	'titre_diagnostic' => 'Diagnostic Subscriptions',
 	'titre_diagnostic_paybox' => 'Diagnostic Paybox/Subscriptions',
+	'titre_statistiques_abonnement' => 'Subscriptions statistiques',
+	'titre_parrainages' => 'Sponsorships',
+	'titre_repartition_des_offres' => 'Breakdown of offers',
 	'tous' => 'All',
-
-	'info_statut_resilie' => 'Terminated',
-	'info_statut_impaye' => 'Unpaid',
-	'info_statut_ok' => 'OK',
-	'info_statut_prepa' => 'command',
-	'info_statut_erreur' => '(error)',
 );
 
 ?>
--- lang/abonnement_fr.php
+++ lang/abonnement_fr.php
@@ -9,6 +9,24 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// A
 	'ajouter_lien_abonnement' => 'Ajouter cet abonnement',
 
+	// C
+	'confirmer_resilier_immediatement' => 'Résilier immédiatement l\'abonnement ? (tous les droits seront coupés)',
+	'confirmer_resilier_echeance' => 'Résilier à l\'échéance ?  (L\'utilisateur pourra normalement utiliser son abonnement jusqu\'à la date d\'échéance)',
+	'confirmer_changement_abonne_uid' => 'Vous devez faire la modification manuelle correspondante chez votre prestataire de paiement <tt>@presta@</tt>.',
+	'confirmer_changement_prix_echeance' => 'Vous devez faire la modification manuelle correspondante chez votre prestataire de paiement <tt>@presta@</tt>.',
+	'confirmer_changement_date' => 'Vous devez faire la modification manuelle correspondante chez votre prestataire de paiement <tt>@presta@</tt>.',
+
+	// D
+	'dont' => 'Dont',
+
+	// E
+	'erreur_date_echeance_ou_fin_obli' => 'Indiquez une date d\'echéance ou une date de fin',
+
+	// G
+	'gratuits_zero' => '0 gratuit',
+	'gratuits_nb' => '@nb@ gratuits',
+	'gratuits_un' => '1 gratuit',
+
 	// I
 	'icone_creer_abonnement' => 'Ajouter un abonnement',
 	'icone_modifier_abonnement' => 'Modifier cet abonnement',
@@ -18,18 +36,15 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'info_nb_abonnements' => '@nb@ abonnements',
 	'info_resilier_immediatement' => 'Résilier immédiatement',
 	'info_resilier_echeance' => 'Résilier à l\'échéance',
-	'confirmer_resilier_immediatement' => 'Résilier immédiatement l\'abonnement ? (tous les droits seront coupés)',
-	'confirmer_resilier_echeance' => 'Résilier à l\'échéance ?  (L\'utilisateur pourra normalement utiliser son abonnement jusqu\'à la date d\'échéance)',
-	'confirmer_changement_abonne_uid' => 'Vous devez faire la modification manuelle correspondante chez votre prestataire de paiement <tt>@presta@</tt>.',
-	'confirmer_changement_prix_echeance' => 'Vous devez faire la modification manuelle correspondante chez votre prestataire de paiement <tt>@presta@</tt>.',
-	'confirmer_changement_date' => 'Vous devez faire la modification manuelle correspondante chez votre prestataire de paiement <tt>@presta@</tt>.',
-	'label_confirmer_modification' => 'Confirmer la modification ?',
-
-	'erreur_date_echeance_ou_fin_obli' => 'Indiquez une date d\'echéance ou une date de fin',
+	'info_statut_resilie' => 'Résilié',
+	'info_statut_impaye' => 'Impayé',
+	'info_statut_ok' => 'OK',
+	'info_statut_prepa' => 'commande',
+	'info_statut_erreur' => '(erreur)',
 
 	// L
 	'label_abonne_uid' => 'Abonne uid',
-	'label_log' => 'Log',
+	'label_confirmer_modification' => 'Confirmer la modification ?',
 	'label_confirm' => 'Confirm',
 	'label_message' => 'Message client',
 	'label_compte' => 'Compte',
@@ -47,18 +62,37 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'label_id_auteur' => 'Id auteur',
 	'label_id_transaction_echeance' => 'Id transaction echeance',
 	'label_id_transaction_essai' => 'Id transaction essai',
+	'label_log' => 'Log',
 	'label_mode' => 'Mode',
 	'label_mode_echeance' => 'Mode echeance',
 	'label_mode_paiement' => 'Mode paiement',
+	'label_mois_de_fin' => 'Mois de fin',
+	'label_nombre' => 'Nombre',
+	'label_non_renouveles' => 'Non renouvelés',
 	'label_offre' => 'Offre',
+	'label_total' => 'Total',
 	'label_prix_echeance' => 'Prix echeance',
+	'label_renouveles' => 'Renouvelés',
+
+	// N
+	'non_commences_zero' => '0 pas encore commencé',
+	'non_commences_un' => '1 pas encore commencé',
+	'non_commences_nb' => '@nb@ pas encore commencés',
+
+	// P
+	'parraines_zero' => '0 parrainé',
+	'parraines_un' => '1 parrainé',
+	'parraines_nb' => '@nb@ parrainés',
 
 	// R
 	'retirer_lien_abonnement' => 'Retirer cet abonnement',
 	'retirer_tous_liens_abonnements' => 'Retirer tous les abonnements',
 
 	// T
+	'texte_abonnements_en_cours' => 'Abonnements en cours',
+	'texte_abonnements_finis_selon_mois' => 'Abonnements finis en fonction des mois',
 	'texte_ajouter_abonnement' => 'Ajouter un abonnement',
+	'texte_aucune_fin_abonnement_nb_mois' => 'Aucune fin d\'abonnement d\'ici @nb@ mois.',
 	'texte_changer_statut_abonnement' => 'Cet abonnement est :',
 	'texte_creer_associer_abonnement' => 'Créer et associer un abonnement',
 	'texte_statut_prepa' => 'En commande',
@@ -67,18 +101,17 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_abonnement' => 'Abonnement',
 	'titre_abonnements' => 'Abonnements',
 	'titre_abonnements_rubrique' => 'Abonnements de la rubrique',
+	'titre_cadeaux' => 'Cadeaux',
+	'titre_encaissements_par_durees' => 'Encaissements par durées',
 	'titre_langue_abonnement' => 'Langue de cet abonnement',
 	'titre_logo_abonnement' => 'Logo de cet abonnement',
 	'titre_diagnostic' => 'Diagnostic Abonnements',
 	'titre_diagnostic_paybox' => 'Diagnostic Paybox/Abonnements',
+	'titre_statistiques_abonnement' => 'Statistiques d’abonnements',
+	'titre_parrainages' => 'Parrainages',
+	'titre_repartition_des_offres' => 'Répartition des offres',
 	'tous' => 'Tous',
 
-
-	'info_statut_resilie' => 'Résilié',
-	'info_statut_impaye' => 'Impayé',
-	'info_statut_ok' => 'OK',
-	'info_statut_prepa' => 'commande',
-	'info_statut_erreur' => '(erreur)',
 );
 
 ?>
